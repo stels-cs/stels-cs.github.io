079d00757a7985eec86ce171b7429896096e3e5a
Иван Недзвецкий <stels-cs@yandex.ru>
2016-07-12 23:09:02 +0300

    Pinder APP

--- src/constants/Page.js
+++
@@ -1,4 +0,0 @@
-export const GET_PHOTOS_REQUEST = 'GET_PHOTOS_REQUEST';
-export const GET_PHOTOS_SUCCESS = 'GET_PHOTOS_SUCCESS';
-export const PAGE_GROUP_LIST = 'groupList';
-export const PAGE_GROUP_VIEW = 'groupView';

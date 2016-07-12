8c616fb5cc67fedd7a49dd279c15b1f00c6e2911
Иван Недзвецкий <stels-cs@yandex.ru>
2016-07-12 23:44:36 +0300

    Fix

--- backend/pinder/public/web-app/src/constants/App.js
+++
@@ -1,3 +0,0 @@
-export const APP_START = 'APP_START';
-export const TEST_MODE = 1;
-export const APP_HTTPS = (window.location.protocol == 'https:') ? 1 : 0;
--- backend/pinder/public/web-app/src/constants/GroupView.js
+++
@@ -1,9 +0,0 @@
-export const SEARCH_PART = 'SEARCH_PART';
-export const SEARCH_ERROR = 'SEARCH_ERROR';
-export const START_SEARCH = 'START_SEARCH';
-export const LIKE = 'LIKE';
-export const SKIP = 'SKIP';
-export const SEARCH_PART_NEXT = 'SEARCH_PART_NEXT';
-export const DROP_AVATAR = 'DROP_AVATAR';
-export const AVATAR_LOADED = 'AVATAR_LOADED';
-export const AVATAR_ERROR = 'AVATAR_ERROR';

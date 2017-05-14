ab39fdb541d025ba5a3e6bc54b3e85a9895fff30
Иван Недзвецкий <stels-cs@yandex.ru>
2017-05-14 14:04:04 +0300

    Remove trash

--- backend/public/web-app/src/tools/share.js
+++
@@ -1,8 +0,0 @@
-export function share() {
-    let url = 'https://vk.com/share.php?';
-    url += 'url=https://vk.com/app5533090';
-    url += '&title=Pinder';
-    url += '&description=Новый способ знакомства в сообществах ВКонтакте';
-    let win = window.open(url, '_blank');
-    win.focus();
-}
